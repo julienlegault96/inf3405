9640aed778c27b249903a5ca12c72cf9c5ed0365
julienlegault96 <julien-3.legault@polymtl.ca>
2019-01-18 14:26:27 -0500

    communication entre plusieurs clients et serveur fait

--- TP1/src/Client.java
+++ TP1/src/Client.java
@@ -1,1 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Enter the IP address of a machine running the capitalize server:");
+        var serverAddress = new Scanner(System.in).nextLine();
+        var socket = new Socket(serverAddress, 5000);
+
+        // Streams for conversing with server
+        var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        var out = new PrintWriter(socket.getOutputStream(), true);
+
+        // Consume and display welcome message from the server
+        System.out.println(in.readLine());
+
+        var scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nEnter a string to send to the server (empty to quit):");
+            var message = scanner.nextLine();
+            if (message == null || message.isEmpty()) {
+                break;
+            }
+            out.println(message);
+            System.out.println(in.readLine());
+        }
+    }
+}
--- TP1/src/Server.java
+++ TP1/src/Server.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -21,6 +25,28 @@ public class Server{
 			this.socket = socket;
 			this.clientNbr = clientNbr;
 			System.out.println("New client #" + clientNbr + " connected at " + socket);			
+		}
+		
+	
+	public void run() {
+		try {
+			var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			var out = new PrintWriter(socket.getOutputStream(), true);
+			
+			out.println("Hello, you are client #" + clientNbr);
+			while(true) {
+				var input = in.readLine();
+				if(input == null || input.isEmpty()) {
+					break;
+				}
+				out.println(input.toUpperCase());
+			}
+		}catch(IOException e) {
+			System.out.println("Error handling client #" + clientNbr);
+		}finally {
+			try {socket.close();} catch(IOException e) {}
+			 System.out.println("Connection with client # " + clientNbr + " closed");
+			}
 		}
 	}
 }
